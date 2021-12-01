a9b656963985ef3f4c715d60cb6c89cd96c66717
x1-digital <94179072+x1-digital@users.noreply.github.com>
2021-12-01 22:11:07 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,8 +3,17 @@ date_default_timezone_set("Asia/Manila");
 $date=date('F j, Y g:i:a');
 
 //mysqli procedural
-$conn=mysqli_connect("sql6.freesqldatabase.com","root","","chatme");
-if(!$conn){
-	die("Connection failed: " . mysqli_connect_error());
+<?php
+$servername = "sql6.freesqldatabase.com";
+$username = "sql6455593";
+$password = "Txh2DzXMT2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
 ?>
